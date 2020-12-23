62d293c5391bc72e9e327d93bf0d2f49b94adb3d
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-23 12:01:54 -0800

    Release 0.1.2

--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-const version = process.env.npm_package_version || '0.1.0'
+|| '0.1.2'const version = process.env.npm_package_version || '0.1.0'
 
 Package.describe({
   name: 'octave',
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   const appName = 'App'
-  const version = '0.1.0'
+  const version = '0.1.2'
   const keywords = appName + '@' + version
 
   return (
