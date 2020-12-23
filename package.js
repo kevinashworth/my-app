e4a8a1725a31afa8ed454ce7fd52bd87dd09c19d
Kevin Ashworth <kevinashworth@yahoo.com>
2020-12-23 12:27:48 -0800

    Release 0.1.6-alpha.0

--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-process.env.npm_package_version || '0.1.5'const version = process.env.npm_package_version || '0.1.0'
+process.env.npm_package_version || '0.1.6-alpha.0'const version = process.env.npm_package_version || '0.1.0'
 
 Package.describe({
   name: 'octave',
